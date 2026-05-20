38a2fa114acde2290201abcd9a63cc0aeed6e947
Rai <rajbolra13@gmail.com>
2026-05-20 22:09:37 +0100

    Test Environment Set Up

--- FirstCSharpScript.cs
+++ FirstCSharpScript.cs
@@ -4,12 +4,21 @@ using System;
 public partial class FirstCSharpScript : Node
 {
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+
+	float time_elasped;
+	float duration;
+    public override void _Ready()
 	{
+		 time_elasped = 0.0f;
+		 duration = 5.0f;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+	public override void _Process(double delta) {
+		time_elasped += (float)delta;
+		if (time_elasped >= duration) {
+			
+            GetTree().ChangeSceneToFile("res://testGround.tscn");
+        }
 	}
 }
